d8b964393cc728b91d67561bc3d2f16d09280e5c
Sune Simonsen <sune@we-knowhow.dk>
2022-11-11 12:12:18 +0100

    Add diffSnapshots and applySnapshotDiff
    This is useful for sending diffs over the wire to mirror state.

--- src/session.js
+++ src/session.js
@@ -2,6 +2,7 @@
 import { subscribables, registerInitial } from "@dependable/state";
 import { storeObservableInCache } from "./storeObservableInCache.js";
 import { restoreObservablesFromCache } from "./restoreObservablesFromCache.js";
+import { deepEqual } from "./equality.js";
 
 /**
  * Returns a snapshot of the current session.
@@ -57,4 +58,73 @@ export const restoreSession = () => {
   const snapshot = JSON.parse(data);
 
   restoreSnapshot(snapshot);
+};
+
+/**
+ * Creates a diff between two session snapshots.
+ *
+ * @param {import('./shared').SessionSnapshot} current a snapshot of the current session
+ * @param {import('./shared').SessionSnapshot} updated a snapshot of the updated session
+ * @return {import('./shared').SessionSnapshotDiff} the diff between the snapshots
+ */
+export const diffSnapshots = (current, updated) => {
+  const diff = {
+    nextId: updated.nextId,
+    observables: {
+      added: {},
+      updated: {},
+      removed: [],
+    },
+  };
+
+  for (const key of Object.keys(current.observables)) {
+    if (key in updated.observables) {
+      const changed = !deepEqual(
+        current.observables[key],
+        updated.observables[key]
+      );
+      if (changed) {
+        diff.observables.updated[key] = updated.observables[key];
+      }
+    } else {
+      diff.observables.removed.push(key);
+    }
+  }
+
+  for (const key of Object.keys(updated.observables)) {
+    if (!(key in current.observables)) {
+      diff.observables.added[key] = updated.observables[key];
+    }
+  }
+
+  return diff;
+};
+
+/**
+ * Applies a snapshot diff to a given session snapshot.
+ *
+ * @param {import('./shared').SessionSnapshot} snapshot a session snapshot to apply the diff to
+ * @param {import('./shared').SessionSnapshotDiff} diff a snapshot diff to apply to the given session snapshot
+ * @return {import('./shared').SessionSnapshot} the resulting session snapshot
+ */
+export const applySnapshotDiff = (snapshot, diff) => {
+  const result = {
+    nextId: diff.nextId,
+    observables: {},
+  };
+
+  const removes = new Set(diff.observables.removed);
+
+  for (const key of Object.keys(snapshot.observables)) {
+    if (!removes.has(key)) {
+      result.observables[key] =
+        diff.observables.updated[key] || snapshot.observables[key];
+    }
+  }
+
+  for (const key of Object.keys(diff.observables.added)) {
+    result.observables[key] = diff.observables.added[key];
+  }
+
+  return result;
 };
